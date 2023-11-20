b08c14275e81f24cd91d9257ccc0c82f59dd0e13
Rithiel do Amaral <145613675+rithieldoamaral@users.noreply.github.com>
2023-11-20 17:54:36 -0300

    Create script_previsao.py

---
+++ previsao_doencas/script_previsao.py
@@ -0,0 +1,29 @@
+# Importar Bibliotecas
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+# Importar Base de Dados
+doenca = pd.read_csv('dados_com_doencas.csv')
+
+# Criar Subconjunto/treino e Teste
+X = doenca.drop(columns=['Doença'])
+y = doenca['Doença']
+X_treino, X_teste, y_treino, y_teste = train_test_split(X, y, test_size=0.05)
+
+# Criar Modelo
+modelo = DecisionTreeClassifier()
+modelo.fit(X_treino, y_treino)
+
+# Nomear as características
+nomes_caracteristicas = X.columns
+
+# Fazer previsões
+previsao = modelo.predict(X_teste)
+
+# Fazer previsão para uma nova instância
+nova_instancia = [[22, 1]]
+previsao2 = modelo.predict(nova_instancia)
+
+print("Previsão para a nova instância:", previsao2)
